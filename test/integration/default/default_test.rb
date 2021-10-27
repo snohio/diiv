19f8ac67d927d542c57a585e426096135763f0a4
Mike Butler <michael.butler@progress.com>
2021-10-27 08:58:49 -0400

    Update for bento/rocky image & fixed LF everywhere

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -11,6 +11,9 @@ if os.family == 'redhat'
   describe port(4443) do
     it { should be_listening }
   end
+    describe port(4040) do
+    it { should be_listening }
+  end
 elsif os.family == 'windows'
   describe port(4040) do
     it { should be_listening }
